a244fbfc8453cafbfcca20d5d590fd9a1c6c0d83
co2co2 <coco8287@hotmail.com>
2018-02-10 00:20:16 -0500

    clone airbnb on react

---
+++ src/components/hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+class Hello extends React.Component {
+  render(){
+    return <div>Hello {this.props.firstname} {this.props.lastname}</div>;
+  }
+}
+export default Hello;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+// import Hello from './components/hello';
+// import registerServiceWorker from './registerServiceWorker';
+// const root =
+// <div>
+// <Hello firstname ="Coco" lastname = "Sheep"/>
+// </div>;
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+// registerServiceWorker();
